524f0bb9ba62e44cba023fd8b2f4d4c12851439e
Viktor Romaniukov <viktorrvi@email.ua>
2025-03-25 11:31:39 +0200

    fix-hw

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,25 +1,19 @@
-import React from 'react'
-import FriendListItem from './FriendListItem'
-import styles from './FriendList.module.css'
+import React from 'react';
+import FriendListItem from './FriendListItem';
+import styles from './FriendList.module.css';
 
-const FriendList = ({friends}) => {
+const FriendList = ({ friends }) => {
   return (
     <div>
-        <ul>
-	        {/* Кількість li залежить від кількості об'єктів в масиві */}
-            <li className={styles.friendList}>
-                    {friends.map(({id, avatar, name, isOnline}) => (
-                        <FriendListItem
-                            key={id}
-                            avatar={avatar}
-                            name={name}
-                            isOnline={isOnline}
-                        />
-                    ))}
- 	        </li>
-        </ul>
+      <ul>
+        {friends.map(({ id, avatar, name, isOnline }) => (
+          <li key={id}>
+            <FriendListItem avatar={avatar} name={name} isOnline={isOnline} />
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default FriendList
+export default FriendList;
